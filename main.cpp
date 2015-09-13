83ee33862742e04fba8d1889690d678b8e7b4450
Thiago Goveia <goveiat@gmail.com>
2015-09-13 18:16:45 -0300

    projeto vazio (netbeans)

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: thiago
+ *
+ * Created on 13 de Setembro de 2015, 17:27
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
